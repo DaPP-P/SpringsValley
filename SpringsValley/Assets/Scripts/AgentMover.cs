6a82012e0b527aa81e10e09e9b143fe27a152e3d
Daniel Prvanov <danielprva@gmail.com>
2023-11-21 17:55:47 +1300

    started on combst tesr build

---
+++ SpringsValley/Assets/Scripts/AgentMover.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentMover : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+
+    [SerializeField]
+    private float maxSpeed = 2, acceleration = 50, deacceleration = 100;
+        
+    [SerializeField]
+    private float currentSpeed = 0;
+    private Vector2 oldMovementInput;
+    public Vector2 MovementInput { get; set; }
+
+    private void awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate() 
+    {
+        if (MovementInput.magnitude > 0 && currentSpeed >= 0)
+        {
+            oldMovementInput = MovementInput;
+            currentSpeed += acceleration * maxSpeed * Time.deltaTime;
+        }
+        else
+        {
+            currentSpeed -= deacceleration * maxSpeed * Time.deltaTime;
+        }
+        currentSpeed = Mathf.Clamp(currentSpeed, 0, maxSpeed);
+        rb2d.velocity = oldMovementInput * currentSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- SpringsValley/Assets/Scripts/playerControl.cs
+++ SpringsValley/Assets/Scripts/playerControl.cs
@@ -7,13 +7,14 @@ public class playerControl : MonoBehaviour
     // Start is called before the first frame update
 
     Rigidbody2D body;
-
     float horizontal;
     float vertical;
-
     float movementDir;
-
     public float speed = 10.0f;
+    public Vector2 mouseDirection = new Vector2();
+    public GameObject weapon;
+    public float offset;
+
 
     void Start()
     {
@@ -29,11 +30,69 @@ public class playerControl : MonoBehaviour
         movementDir = Mathf.Sign(horizontal);
         transform.GetComponent<SpriteRenderer>().flipX = movementDir < 0;
 
-
+        faceMouse();
+        
+        if (Input.GetKey(KeyCode.Mouse0))        
+        {
+            StartCoroutine(hit());
+        }
+    
     }
 
     private void FixedUpdate() {
         body.velocity = new Vector2(horizontal * speed, vertical * speed);
     }
+
+void faceMouse()
+{
+    Vector3 mousePosition = Input.mousePosition;
+    mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+    Vector2 direction = new Vector2(
+        mousePosition.x - transform.position.x,
+        mousePosition.y - transform.position.y
+    );
+    // Calculate the angle of the direction vector
+    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+    // Snap the angle to the nearest 45-degree interval
+    float snappedAngle = Mathf.Round(angle / 45f) * 45f;
+
+    // Convert the snapped angle back to radians
+    float snappedAngleRad = snappedAngle * Mathf.Deg2Rad;
+
+    // Update the direction vector based on the snapped angle
+    direction = new Vector2(Mathf.Cos(snappedAngleRad), Mathf.Sin(snappedAngleRad));
+
+    direction.Normalize();
+
+    mouseDirection = direction;
+    // Check if the angle is greater than 90 degrees or less than -90 degrees
+    if (Vector2.Angle(Vector2.right, direction) > 90f)
+    {
+        // If true, set the player's rotation to the left
+        transform.right = -direction;
+        transform.localScale = new Vector3(-1, 1, 1);
+    } else {
+        // If false, set the player's rotation to the right
+        transform.right = direction;
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+}
+
+IEnumerator hit()
+{
+    print("hit");
+    weapon.SetActive(true);
+
+    // Calculate the weapon position based on the player's position and mouse direction
+    Vector3 weaponPosition = transform.position + offset * new Vector3(mouseDirection.x, mouseDirection.y, 0f);
+    weapon.transform.position = weaponPosition;
+
+    yield return new WaitForSeconds(0.2f);
+
+    weapon.SetActive(false);
+}
+
+
 
 }
