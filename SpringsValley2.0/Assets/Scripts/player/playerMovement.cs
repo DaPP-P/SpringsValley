558af137fc4d990ab2ede23ca121c716edf32795
Daniel Prvanov <danielprva@gmail.com>
2024-12-02 21:11:38 +1300

    slowly adding the work ive already done :/ adding player and layer sorting

---
+++ SpringsValley2.0/Assets/Scripts/player/playerMovement.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    private const float MOVE_SPEED = 3f;     // Player movement speed.
+    public Rigidbody2D rigidbody2D; // Players rigidbody.
+    private Vector3 moveDir; // Player movement direction.
+    public Animator animator; // The animator 
+
+    public SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+    }
+
+    void Update()
+    {
+
+        // Sets movement to zero.
+        float moveX = 0f;
+        float moveY = 0f;
+
+        // Checks movement direction.
+        if(Input.GetKey(KeyCode.W)) {
+            moveY = +1f; 
+        } 
+        if(Input.GetKey(KeyCode.S)) {
+            moveY = -1f;
+        }
+        if(Input.GetKey(KeyCode.D)) {
+            moveX = +1f;
+            spriteRenderer.flipX = false; // Face right
+        }
+        if(Input.GetKey(KeyCode.A)) {
+            moveX = -1f;
+            spriteRenderer.flipX = true; // Face left
+        }
+
+        if (moveY != 0 || moveX != 0) {
+            animator.SetBool("Running", true);
+        } else {
+            animator.SetBool("Running", false);
+        }
+
+        // normalizes the move direction.
+        moveDir = new Vector3(moveX, moveY).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.linearVelocity = moveDir * MOVE_SPEED;
+
+    }
+}
