17111c61fb070bf79e9fca85c32317fce4a4527e
Daniel Prvanov <danielprva@gmail.com>
2024-12-29 11:36:33 +1300

    now displays number of items in inv.

--- SpringsValley2.0/Assets/Inventory.cs
+++ SpringsValley2.0/Assets/Inventory.cs
@@ -9,7 +9,9 @@ public class Inventory : MonoBehaviour
     // Array of Inv Slots and Inv Images
     public GameObject[] invSlots;
     private Image[] invImages;
+    public TextMeshProUGUI[] invSlotCounts;
 
+    public GameObject[] invCountBackground;
 
     public Sprite emptySprite;
     [SerializeField] private Sprite cornSprite;  
@@ -40,20 +42,26 @@ public class Inventory : MonoBehaviour
 
         for (int i = 0; i < invImages.Length; i++)
         {
-            UpdateSlot(invImages[i], itemOrder, i);
+            UpdateSlot(invImages[i], invSlotCounts[i], invCountBackground[i], itemOrder, i);     
         }
     }
 
-    private void UpdateSlot(Image slotImage, List<string> itemOrder, int index)
+    private void UpdateSlot(Image slotImage, TextMeshProUGUI slotCountText, GameObject slotCountBackground, List<string> itemOrder, int index)
     {
-        if (slotImage == null || index >= itemOrder.Count)
+        if (slotImage == null || index >= itemOrder.Count || slotCountText == null)
         {
             slotImage.sprite = emptySprite; // Clear slot if no item
+            slotCountText.enabled = false;
+            slotCountBackground.SetActive(false);
         }
         else
         {
+            slotCountText.enabled = true;
+            slotCountBackground.SetActive(true);
             string itemName = itemOrder[index];
+            int itemCount = PlayerLoot.GetItemAmount(itemName);
             slotImage.sprite = GetSpriteForItem(itemName); // Get corresponding sprite
+            slotCountText.text = itemCount.ToString();
         }
     }
 
