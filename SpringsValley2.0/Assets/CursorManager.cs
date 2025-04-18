2164a85ef2ae66aeb5755bebaeb096a449bda129
Daniel Prvanov <danielprva@gmail.com>
2025-04-18 16:57:28 +1200

    added a cursor and downloadable build for 1.1

---
+++ SpringsValley2.0/Assets/CursorManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    [SerializeField] private Texture2D cursorTextureDefault;
+    [SerializeField] private Vector2 clickPosition = Vector2.zero;
+
+    void Start()
+    {
+        Cursor.SetCursor(cursorTextureDefault, clickPosition, CursorMode.Auto);
+    }
+}
